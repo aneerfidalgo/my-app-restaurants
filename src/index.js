2b44cabf3c92472c234d8fceae7520c5cf7719a0
Aneer Fidalgo <aneer@Aneers-MacBook-Air.local>
2022-05-26 13:15:23 -0400

    added some style on the index

--- src/index.js
+++ src/index.js
@@ -2,27 +2,37 @@ import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
   container: {
-    marginVertical: 20,
     flex: 1,
-    //backgroundColor: "#fff",
+    width: "100%",
+    backgroundColor: "#00000032",
     alignItems: "center",
     justifyContent: "center",
+    marginHorizontal: 0,
   },
   restaurantCard: {
     width: "100%",
-    marginVertical: 8,
-    backgroundColor: "fff",
+    marginVertical: 16,
+    backgroundColor: "#fff",
     color: "#444",
     borderRadius: 4,
   },
   restaurantsName: {
-    fontsize: 24,
+    marginHorizontal: 12,
+    fontSize: 24,
     fontWeight: "600",
-    marginVertical: 15,
+    marginTop: 15,
   },
   cuisine: {
+    marginHorizontal: 12,
     fontSize: 16,
+    fontWeight: "100",
+    color: "#ff3333",
+  },
+  address: {
+    marginHorizontal: 12,
+    fontSize: 14,
     fontWeight: "300",
     color: "#303030",
+    marginBottom: 8,
   },
 });
